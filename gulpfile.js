391c400de5095fc8aca321e4e7b489122fc5e4bd
Thomas Gaudin <thomas.gaudin@centraliens-lille.org>
2017-03-03 13:46:39 +0100

    Add reloadCss task; fix gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -8,8 +8,8 @@ var download = require('gulp-download-stream');
 const paths = {
   htmlSrc: 'src/index.html',
   mdSrc: 'src/presentation.md',
+  cssSrc: 'src/assets/css/stylesheet.css',
   assetsDir: 'src/assets',
-  cssDir: 'src/assets/css',
   jsDir: 'src/assets/js',
   imgDir: 'src/assets/img',
   distDir: 'dist',
@@ -21,28 +21,50 @@ gulp.task('compileHtml', function () {
     .pipe(gulp.dest(paths.distDir));
 });
 
-gulp.task('copyAssets', function() {
-    return gulp.src(paths.assetsDir + '/**/*')
+gulp.task('compileCss', function () {
+  return gulp.src(paths.cssSrc)
+    .pipe(gulp.dest(paths.distDir + '/assets/css'));
+});
+
+gulp.task('copyImg', function() {
+    return gulp.src(paths.imgDir + '/**/*', {base: paths.assetsDir})
       .pipe(gulp.dest(paths.distDir + '/assets'));
 });
 
-gulp.task('build',['compileHtml', 'copyAssets'], function() {
-    return;
+gulp.task('copyJs', function() {
+    return gulp.src(paths.jsDir + '/**/*', {base: paths.assetsDir})
+      .pipe(gulp.dest(paths.distDir + '/assets'));
 });
+
+gulp.task('copyAssets',['copyImg', 'copyJs']);
+
+gulp.task('build',['compileHtml', 'compileCss', 'copyAssets']);
 
 gulp.task('md-watch',['build'], function(done) {
     browserSync.reload();
     done();
 });
 
+gulp.task('html-watch',['build'], function(done) {
+    browserSync.reload();
+    done();
+});
+
+gulp.task('css-watch', ['compileCss'], function(done) {
+    browserSync.reload();
+    done();
+});
+
 gulp.task('watch',['build'], function() {
-    browserSync.init(paths.distDir + '/**/*', {
+    browserSync.init({
       server: {
         baseDir: paths.distDir
       }
     });
 
     gulp.watch(paths.mdSrc, ['md-watch']);
+    gulp.watch(paths.htmlSrc, ['html-watch']);
+    gulp.watch(paths.cssSrc, ['css-watch']);
 });
 
 gulp.task('deploy', ['build'], function() {
